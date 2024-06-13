463fc798e14bb0bffbb6dea296c729d25dd420cc
Thomas Hofting <thofting@c4c5c3.42berlin.de>
2024-06-13 22:14:05 +0200

    Added dummy location to the main to migrate new config structure

--- testmain.cpp
+++ testmain.cpp
@@ -1,6 +1,7 @@
 #include "server/Server.hpp"
 #include <vector>
 #include "parsers/ConfigParse.hpp"
+#include <cstdlib>
 
 int main() {
 	//std::vector<t_server> configs;
@@ -12,6 +13,12 @@ int main() {
 		std::cout <<e.what() << std::endl;
 	}
 		Config[0].host = "127.0.0.1";
+		location dummyLocation;
+		dummyLocation._httpMethods = GET & POST & HEAD;
+		// dummyLocation._errorPage = 
+		dummyLocation._path = std::getenv("OLDPWD") + std::string("/content");
+		// dummyLocation._redirect =
+		Config[0].locations["/"] = dummyLocation;
 		Server *sock = new Server(Config[0]);
 		sock->CheckForConnections();
 		//servernbr++;
