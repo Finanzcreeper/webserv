5835bdcbf20dd3cf55eae6449e9ba39b9eb089b5
Nico Reher <nreher@student.42berlin.de>
2024-07-10 17:06:48 +0200

    added finished reading check for no request body

--- parsers/httpParser.cpp
+++ parsers/httpParser.cpp
@@ -30,6 +30,9 @@ void handleHeader(Request &request, size_t endOfBlock) {
 	GetRequestedPath(request);
 	decapitalizeHeaderFields(request.HeaderBuffer);
 	extractHeaderFields(request);
+	if (request.HeaderFields.find("content-length")->second.empty() == true && request.HeaderFields.find("transfer-encoding")->second.empty() == true) {
+		request.requestCompletlyRecieved = true;
+	}
 }
 
 void GetRequestType(Request& request) {
@@ -85,6 +88,7 @@ void extractHeaderFields(Request& request) {
 	SearchedHeaderFields.push_back("transfer-encoding");
 	SearchedHeaderFields.push_back("user-agent");
 	SearchedHeaderFields.push_back("content-type");
+	SearchedHeaderFields.push_back("content-length")
 
 
 	long unsigned int i = 0;
